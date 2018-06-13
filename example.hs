8202d91acf934b26956fe3252359651724954d86
mark <mah6@williams.edu>
2018-06-13 04:30:44 +0100

    updated example

--- example.hs
+++ example.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Graphics.BokeHS.Models
+import Graphics.BokeHS.List
 import Graphics.BokeHS.Serialize (emitPlotHTML, defaultToolbar)
 import Data.ByteString.Lazy as BS
 import Control.Monad
@@ -31,6 +32,31 @@ myData = CDS {
                      19.083333333333334,
                      13.25] 
 
+myData2 = Prelude.zip xcols ycols
+        where
+            xcols =  [-0.5,
+                     1.8333333333333335,
+                     4.166666666666667,
+                     6.5,
+                     8.833333333333334,
+                     11.166666666666668,
+                     13.5,
+                     15.833333333333336,
+                     18.166666666666668,
+                     20.5]
+            ycols = [3.75,
+                     4.916666666666667,
+                     6.083333333333334,
+                     7.25,
+                     8.416666666666667,
+                     9.583333333333334,
+                     10.75,
+                     11.916666666666668,
+                     13.083333333333334,
+                     14.25] 
+
+
+
 myPlot :: Plot
 myPlot = Plot{
        backgroundFill = Lavender,
@@ -51,8 +77,11 @@ myPlot = Plot{
         dataSource = myData, glyph = lin, vie = CDSView}
         lin = Line Purple (Field "x") (Field "y")
 
+myPlot2 :: Plot
+myPlot2 = addLine myPlot myData2 Purple
+
 main :: IO ()
 main = do
-    plotHTML <- emitPlotHTML myPlot
+    plotHTML <- emitPlotHTML myPlot2
     BS.writeFile "sample.html" plotHTML
     void $ system "firefox --new-window sample.html"    
